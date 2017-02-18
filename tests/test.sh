54d49e396d9c755f56fee57691429aa45617d078
Tamas Molnar <tmolnar0831@gmail.com>
2017-02-18 09:08:59 +0100

    Refactoring Ansible tests

--- tests/test.sh
+++ tests/test.sh
@@ -3,7 +3,15 @@
 SOURCE="${BASH_SOURCE[0]}"
 RDIR="$( dirname "$SOURCE" )"
 cd $RDIR
-echo "localhost ansible_connection=local" > inventory
+echo "localhost ansible_connection=local ansible_user=root" > inventory
 
+# Testing the syntax
 ansible-playbook --syntax-check -i inventory test.yml
-ansible-playbook -i inventory test.yml -u root --become-user=root
+
+# First run
+ansible-playbook -i inventory test.yml
+
+# Idempotence
+ansible-playbook -i inventory test.yml | grep -q 'changed=0.*failed=0' || exit 1
+
+
